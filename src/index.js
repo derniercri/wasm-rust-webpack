8301577966246b078b57259a6950bbc4648e5b60
Guillaume Bailleul <laibulle@gmail.com>
2017-11-20 01:34:41 +0100

    thread error demo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const wasm = require('./main.rs')
 
 wasm.initialize({noExitRuntime: true}).then(module => {
   const multiply = module.cwrap('multiply', 'number', ['number', 'number'])
+  const threadMultiply = module.cwrap('thread_multiply', 'number', ['number', 'number'])
   document.write("<p>Result of 3 * 2 from Web Assembly = " + multiply(3, 2) + "</p>")
   document.write("<strong>Awesome! Tomorrow we will try substractions.</strong>")
+  document.write("<p>Threads are not supported yet. They throw an error in console.</p>")
+  threadMultiply(3, 2)
 })
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
+use std::thread;
+
 fn main() {
   println!("Hello, world!");
 }
 
 #[no_mangle]
 pub fn multiply(a: i32, b: i32) -> i32 {
-  return a * b
+  a * b
+}
+
+// This is not supported yet
+#[no_mangle]
+pub fn thread_multiply(a: i32, b: i32) -> i32 {
+  let child = thread::spawn(move || {
+    a * b
+  });
+  let res = child.join();
+  res.unwrap()
 }
